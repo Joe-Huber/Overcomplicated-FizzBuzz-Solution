4852286b069761ab501e3b870802f6fe17d65044
Joe-Huber <joseph.robert.huber@gmail.com>
2026-03-09 23:38:28 -0500

    chore: set up base test methods

---
+++ src/main/java/FizzBuzzTests.java
@@ -0,0 +1,15 @@
+package main.java;
+
+public class FizzBuzzTests {
+    public static void main(String[] args) {
+
+    }
+
+    private static void standardRuleTests(){
+
+    }
+
+    private static void customRuleTests(){
+
+    }
+}
